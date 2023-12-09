e9dca45ead5ef1539ae8ae68d68cc4bb2e28a5ef
Pratish <150532774+pjgit2@users.noreply.github.com>
2023-12-09 15:34:28 +0000

    Updated pyodbc connection details

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 
+#Reference starts for setting pyodbc connection and running SQL:
+# https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver16
+
+import pyodbc
+
+server = 'pgmdbsrv.database.windows.net'
+database = 'pgmdb'
+username = 'addmin'
+password = 'Login123'
+driver = '{ODBC Driver 18 for SQL Server}'
+
+conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+conn = pyodbc.connect(conn_str)
+
+cursor = conn.cursor()
+cursor.execute(SQL_QUERY)
+
+#Reference ends for setting pyodbc connection and running SQL:
