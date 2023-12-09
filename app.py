5c2d15491e8af6f3f5956930786d27ca39b7eba3
Pratish <150532774+pjgit2@users.noreply.github.com>
2023-12-09 15:41:58 +0000

    Added flask import and app run statements; included attribution

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+#Referenced from the following link to import flask:
+#https://elearning.dbs.ie/mod/page/view.php?id=1445635
+
+from flask import Flask
+app = Flask(__name__)
+
+#Reference ends to import flask:
 
 #Reference starts for setting pyodbc connection and running SQL:
 # https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver16
@@ -17,4 +24,9 @@ conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
 cursor.execute(SQL_QUERY)
 
-#Reference ends for setting pyodbc connection and running SQL:
+#Reference ends for setting pyodbc connection and running SQL.
+
+#Referenced from the following link to run the app
+#https://elearning.dbs.ie/mod/page/view.php?id=1445635
+if __name__ == "__main__":
+ app.run(host='0.0.0.0', port='8080') # indent this line
