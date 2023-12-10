ebf03082a13b33835f1609bc3a4164ce68823b5a
Pratish <150532774+pjgit2@users.noreply.github.com>
2023-12-10 23:10:49 +0000

    Comment changes

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-#Referenced from the following link to import flask:
+#References:
 #https://elearning.dbs.ie/mod/page/view.php?id=1445635
 #https://flask.palletsprojects.com/en/2.3.x/quickstart
 
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-#Reference ends to import flask:
-
-# https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver16
+#Reference:
+#https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver16
 
 import pyodbc
 server = 'pgmdbsrv.database.windows.net'
@@ -20,7 +19,7 @@ conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
 
-#Idea from https://elearning.dbs.ie/mod/page/view.php?id=1445635
+#Idea taken from https://elearning.dbs.ie/mod/page/view.php?id=1445635
 #And https://elearning.dbs.ie/mod/page/view.php?id=1445637 
 #And https://medium.com/techcrush/how-to-render-html-file-in-flask-3fbfb16b47f6
 @app.route("/report")#URL leading to method
@@ -53,9 +52,9 @@ def delete():
         conn.commit()
     return render_template('delete.html')
 
-#Referenced from the following link to run the app
+#Reference:
 #https://elearning.dbs.ie/mod/page/view.php?id=1445635
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') # indent this line
 
-# Adding a comment to test change update to VM
+
