5aabeb887ca608d27353f7182c13b8ac1fa89d57
Pratish <150532774+pjgit2@users.noreply.github.com>
2023-12-10 21:37:33 +0000

    added methods -- line 37

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def report(): # Name of the method
 def index(): # Name of the method
  return render_template('index.html')
 
-@app.route("/create")#URL leading to method
+@app.route("/create",methods=['POST'])
 def create():
     if request.method == 'POST':
         name = request.form['name']
